2c81f63b797505c0cb79d46cd690a8cd3bcc3a54
Marek Gierlicki <marek.gierlicki@netguru.com>
2021-10-10 20:32:55 +0200

    Generate invoice pdf

---
+++ config/initializers/grover.rb
@@ -0,0 +1,11 @@
+# config/initializers/grover.rb
+Grover.configure do |config|
+  config.root_url = 'http://localhost:3000/'
+  # config.options = {
+  #   debug: {
+  #     headless: false,  # Default true. When set to false, the Chromium browser will be displayed
+  #     devtools: false    # Default false. When set to true, the browser devtools will be displayed.
+  #   }
+  # }
+end
+
